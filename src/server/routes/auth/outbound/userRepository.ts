54566f738509d0c98453f0d9dd19729d4834e716
Marcus Rodriguez <lkstecumseh@gmail.com>
2025-10-29 14:02:40 -0700

    Adding create user repository function to add new user to db

--- src/server/routes/auth/outbound/userRepository.ts
+++ src/server/routes/auth/outbound/userRepository.ts
@@ -3,7 +3,7 @@ import { queryDbConnection, getOneRowResult } from '../../sql';
 import { handleError, createErrorResponse, createSuccessResponse } from '../../errors';
 import { ErrorKeys } from '../../errors/errors.types';
 import { ApiResponse } from '../../apiResponse.types';
-import { UserSession } from '../types';
+import { UserSession, CreatedUserResult } from '../types';
 
 export async function getUserSession(
   token: string
@@ -23,5 +23,45 @@ export async function getUserSession(
       ErrorKeys.GENERAL_SERVER_ERROR,
       error instanceof Error ? error.message : 'Unknown error occurred'
     );
+  }
+};
+
+export async function createUser(
+  username: string,
+  email: string,
+  password: string
+): Promise<CreatedUserResult> {
+  const query = `
+    INSERT INTO users
+    (username, email, password)
+    VALUES
+    ($1, $2, $3)
+    RETURNING id
+  `;
+  const values = [username, email, password];
+  
+  try {
+    const result = await queryDbConnection(query, values);
+    return getOneRowResult(result);
+  }
+
+  catch (error: any) {
+    const {code, constraint} = error;
+    let errorMessage = ErrorKeys.GENERAL_SERVER_ERROR;
+    
+    if (code && code === '23505') {
+      switch (constraint) {
+      case 'users_username_key':
+        errorMessage = ErrorKeys.USERNAME_TAKEN;
+        break;
+      case 'users_email_key':
+        errorMessage = ErrorKeys.EMAIL_TAKEN;
+        break;
+      default:
+        errorMessage = ErrorKeys.GENERAL_SERVER_ERROR;
+        break;
+      }
+    }
+    throw new ClientError(errorMessage, code, constraint);
   }
 };
